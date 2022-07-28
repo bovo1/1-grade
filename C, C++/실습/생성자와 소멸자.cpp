9029d39262c74ed24fd237f671c8289b687c6d58
bovo1 <110110403+bovo1@users.noreply.github.com>
2022-07-28 23:26:44 +0900

    Add files via upload

---
+++ C, C++/실습/생성자와 소멸자.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class samplePoint
+{
+	private:
+	int x;
+	int y;
+	public:
+	samplePoint();
+	samplePoint(int x1, int y1);
+	~samplePoint();
+};
+
+samplePoint::samplePoint()
+{
+	x =1;
+	y = 1;
+	cout<<'('<<x<<','<<y<<')'<<"점 생성"<<endl;
+}
+samplePoint::samplePoint(int x1, int y1)
+{
+	x = x1;
+	y = y1;
+	cout<<'('<<x<<','<<y<<')'<<"점 생성"<<endl;
+}
+samplePoint::~samplePoint()
+{
+	cout<<'('<<x<<','<<y<<')'<<"점 파괴"<<endl;
+}
+int main()
+{
+	samplePoint p1;
+	samplePoint p2(2,5);
+	cout<<"이 프로그램은 종료 되었습니다."<<endl;
+	return 0;
+}
